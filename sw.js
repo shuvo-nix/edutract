ff64231390b28c73d0dc1a94cbf05c9a8780956b
ShuvoniX <shuvonix@mail.com>
2026-09-09 23:31:23 +0200

    Theme-aware logo, PNG favicon, installable PWA with EduTract name and app icon (2/2)

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function (event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function (event) {
+  /* pass-through: always use the network, no caching */
+});
